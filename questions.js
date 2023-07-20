9cfba122731c890618f43987c45810e59b3b099f
Bhawna-1811 <91896743+Bhawna-1811@users.noreply.github.com>
2023-07-20 10:19:15 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,123 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Which spell is used to transform objects into birds?",
+    answer: "Avis",
+    options: ["Wingardium Leviosa", "Avada Kedavra", "Avis", "Expelliarmus"]
+  },
+  {
+    numb: 2,
+    question: "Which spell is used to create a powerful shield?",
+    answer: "Protego Maxima",
+    options: ["Lumos", "Alohomora", "Protego Maxima", "Accio"]
+  },
+  {
+    numb: 3,
+    question: "Which spell is used to conjure a Patronus?",
+    answer: "Expecto Patronum",
+    options: ["Crucio", "Expelliarmus", "Expecto Patronum", "Impedimenta"]
+  },
+  {
+    numb: 4,
+    question: "Which spell is used to disarm an opponent?",
+    answer: "Expelliarmus",
+    options: ["Incendio", "Stupefy", "Expelliarmus", "Reparo"]
+  },
+  {
+    numb: 5,
+    question: "Which spell is used to unlock doors?",
+    answer: "Alohomora",
+    options: ["Alohomora", "Riddikulus", "Wingardium Leviosa", "Incendio"]
+  },
+  {
+    numb: 6,
+    question: "Which spell is used to control the actions of another person?",
+    answer: "Imperio",
+    options: ["Episkey", "Crucio", "Imperio", "Avada Kedavra"]
+  },
+  {
+    numb: 7,
+    question: "Which spell is used to mend or repair broken objects?",
+    answer: "Reparo",
+    options: ["Expelliarmus", "Stupefy", "Reparo", "Protego"]
+  },
+  {
+    numb: 8,
+    question: "Which spell is used to summon an object to the caster?",
+    answer: "Accio",
+    options: ["Impedimenta", "Accio", "Avada Kedavra", "Expecto Patronum"]
+  },
+  {
+    numb: 9,
+    question: "Which spell is used to create light at the tip of the wand?",
+    answer: "Lumos",
+    options: ["Reparo", "Expelliarmus", "Lumos", "Crucio"]
+  },
+  {
+    numb: 10,
+    question: "Which spell is used to repel or push away an object or opponent?",
+    answer: "Impedimenta",
+    options: ["Stupefy", "Expelliarmus", "Impedimenta", "Imperio"]
+  },
+  {
+    numb: 11,
+    question: "Which spell is used to conjure a powerful blast of wind?",
+    answer: "Ventus",
+    options: ["Avis", "Protego", "Ventus", "Avada Kedavra"]
+  },
+  {
+    numb: 12,
+    question: "Which spell is used to create a ring of fire around the caster?",
+    answer: "Incendio",
+    options: ["Alohomora", "Expelliarmus", "Incendio", "Reparo"]
+  },
+  {
+    numb: 13,
+    question: "Which spell is used to levitate objects?",
+    answer: "Wingardium Leviosa",
+    options: ["Lumos", "Alohomora", "Wingardium Leviosa", "Accio"]
+  },
+  {
+    numb: 14,
+    question: "Which spell is used to transform an object into a small animal?",
+    answer: "Duro",
+    options: ["Impedimenta", "Expecto Patronum", "Duro", "Crucio"]
+  },
+  {
+    numb: 15,
+    question: "Which spell is used to freeze or immobilize a target?",
+    answer: "Petrificus Totalus",
+    options: ["Episkey", "Petrificus Totalus", "Reparo", "Stupefy"]
+  },
+  {
+    numb: 16,
+    question: "Which spell is used to make objects disappear?",
+    answer: "Evanesco",
+    options: ["Expelliarmus", "Evanesco", "Protego", "Avada Kedavra"]
+  },
+  {
+    numb: 17,
+    question: "Which spell is used to create a silent barrier around an area?",
+    answer: "Muffliato",
+    options: ["Lumos", "Muffliato", "Crucio", "Expecto Patronum"]
+  },
+  {
+    numb: 18,
+    question: "Which spell is used to create a small flame at the tip of the wand?",
+    answer: "Incendio",
+    options: ["Alohomora", "Riddikulus", "Incendio", "Expelliarmus"]
+  },
+  {
+    numb: 19,
+    question: "Which spell is used to erase the memory of a specific event?",
+    answer: "Obliviate",
+    options: ["Accio", "Obliviate", "Impedimenta", "Avada Kedavra"]
+  },
+  {
+    numb: 20,
+    question: "Which spell is used to create an unbreakable vow?",
+    answer: "Vowelicera",
+    options: ["Protego", "Vowelicera", "Expecto Patronum", "Episkey"]
+  }
+];
